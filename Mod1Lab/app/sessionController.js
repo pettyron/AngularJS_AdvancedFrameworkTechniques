52127bfdf0aebd1f5e5c22486fdee8a973b7f3ff
Ronnie Petty <rpetty.developer@gmail.com>
2018-03-03 22:57:26 -0600

    build service portion of Mod1Lab

---
+++ Mod1Lab/app/sessionController.js
@@ -0,0 +1,24 @@
+angular.module('app').controller('sessionController', ['sessionService', sessionController]);
+
+function sessionController(sessionService) {
+    var vm = this;
+
+    vm.getServiceSession = function () {
+        vm.model = {
+            name: sessionService.get('name'),
+            nickname: sessionService.get('nickname'),
+            status: 'Retrieved by service on ' + new Date()
+        };
+    }
+    vm.setServiceSession = function () {
+        sessionService.save('name', vm.model.name);
+        sessionService.save('nickname', vm.model.nickname);
+        vm.getServiceSession();
+    }
+    vm.clearServiceSession = function () {
+        sessionService.clear();
+        vm.getServiceSession();
+    }
+}
+
+// console.log('sessionController');
